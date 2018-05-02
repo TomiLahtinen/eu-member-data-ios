0d8f66970bddf35f387f3743a9586e86d10c5a29
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2018-05-02 14:25:22 +0300

    Removed un-needed example test classes

--- eu-member-dataUITests/CountriesViewControllerTest.swift
+++ eu-member-dataUITests/CountriesViewControllerTest.swift
@@ -28,7 +28,7 @@ class CountriesViewControllerTest: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testSortingByAreaDown() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let app = XCUIApplication()
